3cab78e4d1834d761304094f4bae9bbf799eb4f7
Lorena <111245669+LorenaIdelfonso@users.noreply.github.com>
2023-12-16 05:59:53 -0300

    update ui colors

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function activate(e) {
     root.style.setProperty("--color-p1", "#1788cf");
     root.style.setProperty("--color-p2", "#0f7abd");
     root.style.setProperty("--color-p3", "#004E80");
-    root.style.setProperty("--color-n1", "#ffffff");
-    root.style.setProperty("--color-n8", "#000000");
+    root.style.setProperty("--color-n1", "#F5F7FA");
+    root.style.setProperty("--color-n8", "#F5F7FA");
   } else if (src.includes("2")) {
     root.style.setProperty("--color-p1", "#B4B7B9");
     root.style.setProperty("--color-p2", "#959A9D");
@@ -20,11 +20,11 @@ function activate(e) {
     root.style.setProperty("--color-n1", "#000000");
     root.style.setProperty("--color-n8", "#000000");
   } else {
-    root.style.setProperty("--color-p1", "#E60C2C");
-    root.style.setProperty("--color-p2", "#C70522");
-    root.style.setProperty("--color-p3", "#330007");
-    root.style.setProperty("--color-n1", "#ffffff");
-    root.style.setProperty("--color-n8", "#ffffff");
+    root.style.setProperty("--color-p1", "#BD0F0F");
+    root.style.setProperty("--color-p2", "#910808");
+    root.style.setProperty("--color-p3", "#1A0000");
+    root.style.setProperty("--color-n1", "#F5F7FA");
+    root.style.setProperty("--color-n8", "#F5F7FA");
   }
 };
 
